4f7f45a2f6b8cb27e3f8efcafadc9a1a33e906f7
chiajee <leechia.jee97@gmail.com>
2019-11-18 16:46:39 +0800

    jee commit

--- app/src/main/java/com/example/billsplittingapp/MainActivity.java
+++ app/src/main/java/com/example/billsplittingapp/MainActivity.java
@@ -1,31 +1,88 @@
 package com.example.billsplittingapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     Fragment selectFragment;
     FirebaseAuth auth = FirebaseAuth.getInstance();
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         final Fragment quickSplit = new QuickSplitMain();
         final BottomNavigationView menu = findViewById(R.id.navigation);
+
+        /*Drawer Menu */
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+//                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams
+//                .WRAP_CONTENT);
+//        params.addRule(RelativeLayout.BELOW,R.id.toolbar);
+
+
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar,
+                R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        int id = menuItem.getItemId();
+                        switch (id){
+                            case R.id.nav_logout:
+                                //logout
+                                Toast.makeText(getApplicationContext(),
+                                        "Logout",Toast.LENGTH_SHORT).show();
+                            case R.id.nav_change_password:
+                                //change pw
+                                Toast.makeText(getApplicationContext(),
+                                        "Change Password",Toast.LENGTH_SHORT).show();
+                            default:
+                                return true;
+                        }
+                    }
+                }
+        );
+
         menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -38,15 +95,16 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.navigation_quick_split:
                         selectFragment = quickSplit;
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
                         setTitle("Quick Split");
                         break;
                     case R.id.navigation_notification:
-                        setTitle("Notificatin");
+                        setTitle("Notification");
                         break;
                     default:
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
+
 
                 return true;
             }
@@ -55,7 +113,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onBackPressed() {
+
         super.onBackPressed();
         finishAffinity();
     }
--- app/src/main/java/com/example/billsplittingapp/RegisterActivity.java
+++ app/src/main/java/com/example/billsplittingapp/RegisterActivity.java
@@ -1,14 +1,92 @@
 package com.example.billsplittingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+private FirebaseAuth auth;
+private EditText etemail,etname,etpw,etconfirmpw;
+private Button btnRegister;
+private TextView btnLogin;
+private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etemail = (EditText)findViewById(R.id.email_input);
+        etname = (EditText)findViewById(R.id.name_input);
+        etpw = (EditText)findViewById(R.id.password_input);
+        etconfirmpw = (EditText)findViewById(R.id.confirm_password_input);
+        btnRegister = (Button)findViewById(R.id.register_button);
+        btnLogin = (TextView)findViewById(R.id.btn_to_login);
+        auth = FirebaseAuth.getInstance();
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etemail.getText().toString().trim();
+                String name = etname.getText().toString().trim();
+                String pw = etpw.getText().toString().trim();
+                String confirmpw = etconfirmpw.getText().toString().trim();
+                if(email.equals("") || name.equals("")
+                        || pw.equals("") || confirmpw.equals("")){
+                    Toast.makeText(getApplicationContext(),
+                            "Field(s) are empty",
+                            Toast.LENGTH_SHORT).show();
+                } else if(!pw.equals(confirmpw)){
+                    Toast.makeText(getApplicationContext(),
+                            "Passwords are not the same",
+                            Toast.LENGTH_SHORT).show();
+                } else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    btnRegister.setVisibility(View.GONE);
+                    auth.createUserWithEmailAndPassword(email,pw)
+                            .addOnCompleteListener(RegisterActivity.this,
+                                    new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            Toast.makeText(RegisterActivity.this,
+                                                    "Register Success"
+                                            , Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.GONE);
+                                            if(!task.isSuccessful()){
+                                                Toast.makeText(RegisterActivity.this,
+                                                        "Authentication Fail"
+                                                                +task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                            } else{
+                                                Intent i = new Intent(RegisterActivity.this
+                                                        ,LoginActivity.class);
+                                                startActivity(i);
+                                                finish();
+                                            }
+                                        }
+                                    });
+                }
+            }
+        });
     }
 }
