fe0ce4a7a067bcc11a185b1400cdb9df2fe4c345
Syakir <m19syakir97@gmail.com>
2019-12-13 13:15:35 +0800

    Expense layout im dumb

--- app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
+++ app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
@@ -1,19 +1,46 @@
 package com.example.billsplittingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class ExpenseAddNew extends AppCompatActivity {
+
     String groupId;
     String groupName;
+    Button btnSave;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    DocumentReference doc;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.method_of_split);
+        setContentView(R.layout.groups_expense);
 
         groupId = getIntent().getStringExtra("groupId");
         groupName = getIntent().getStringExtra("groupName");
+
+        Intent intent = getIntent();
+        HashMap<String, String> hashMap = (HashMap<String, String>) intent.getSerializableExtra("hashMap");
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
