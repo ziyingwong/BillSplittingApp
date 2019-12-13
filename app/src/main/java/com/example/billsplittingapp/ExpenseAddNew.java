d854e4b8e660208f09d8e7be9cfdf318e1d045d2
Syakir <m19syakir97@gmail.com>
2019-12-13 19:49:31 +0800

    Expense layout

--- app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
+++ app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,16 +15,20 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ExpenseAddNew extends AppCompatActivity {
 
     String groupId;
     String groupName;
     Button btnSave;
+    TextView tvGroupName;
+    EditText tvBillName;
+    //EditText tvPayer;
+    EditText tvPrice;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth auth = FirebaseAuth.getInstance();
-    DocumentReference doc;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,13 +38,36 @@ public class ExpenseAddNew extends AppCompatActivity {
         groupId = getIntent().getStringExtra("groupId");
         groupName = getIntent().getStringExtra("groupName");
 
-        Intent intent = getIntent();
-        HashMap<String, String> hashMap = (HashMap<String, String>) intent.getSerializableExtra("map");
+        tvGroupName = findViewById(R.id.tvGroupName);
+        tvBillName = findViewById(R.id.tvBillName);
+        //tvPayer = findViewById(R.id.tvPayer);
+        tvPrice = findViewById(R.id.tvPrice);
 
-        btnSave = (Button) findViewById(R.id.btnSave);
+        tvGroupName.setText(groupName);
+
+        Intent intent = getIntent();
+        //final HashMap<String, String>[] hashMap = new HashMap[]{(HashMap<String, String>) intent.getSerializableExtra("map")};
+
+        btnSave = findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                DocumentReference doc = db.collection("Groups").document();
+                Map<String, String> data = new HashMap<>();
+
+                data.put("billId", doc.getId());
+                data.put("billName", tvBillName.getText().toString());
+                data.put("payer", auth.getCurrentUser().getUid());
+                data.put("price", tvPrice.getText().toString());
+
+                db.collection("Groups").document(groupId).collection("Payment").document(doc.getId()).set(data);
+
+
+                /*double price = Double.parseDouble(tvPrice.getText().toString());
+                if (hashMap[0] == null){
+                    hashMap[0] = new HashMap<>();
+                }*/
 
             }
         });
