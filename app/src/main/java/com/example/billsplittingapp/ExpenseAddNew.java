c2ee2d04f2efdc0a2fc51cec48b495af67793bd5
Syakir <m19syakir97@gmail.com>
2019-12-14 18:49:26 +0800

    Expense layout (Uncomment splitAmount if you want too try splitting method mirul)

--- app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
+++ app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,11 +35,14 @@ public class ExpenseAddNew extends AppCompatActivity {
     String groupId;
     String groupName;
     Button btnSave;
+    Button btnSplit;
     TextView tvGroupName;
     EditText tvBillName;
     EditText tvPrice;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth auth = FirebaseAuth.getInstance();
+    Map<String, Double> splitAmount;
+    ArrayList<String> splitUser;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,50 +51,81 @@ public class ExpenseAddNew extends AppCompatActivity {
 
         groupId = getIntent().getStringExtra("groupId");
         groupName = getIntent().getStringExtra("groupName");
-
+//        splitAmount = (HashMap<String, Double>) getIntent().getSerializableExtra("user"); //GET FROM AMIRUL
+        splitUser = getIntent().getStringArrayListExtra("splitUser");
         tvGroupName = findViewById(R.id.tvGroupName);
         tvBillName = findViewById(R.id.tvBillName);
         tvPrice = findViewById(R.id.tvPrice);
 
         tvGroupName.setText(groupName);
 
-        Intent intent = getIntent();
-
         btnSave = findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 DocumentReference doc = db.collection("Groups").document();
-
-
-                Map<String, Object> data = new HashMap<>();
-                final Map<String, Double>[] userArray = new Map[]{new HashMap<>()};
-                List array = new ArrayList();
+                double total = Double.parseDouble(tvPrice.getText().toString());
 
                 db.collection("Groups").document(groupId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if (documentSnapshot.exists()) {
-                            userArray[0] = (HashMap) documentSnapshot.get("user");
+
+                            Map<String, Object> data = new HashMap<>();
+
+                            Map<String, Double> user;
+                            ArrayList<String> userArray;
+
+                            user = (HashMap) documentSnapshot.get("user");
+                            userArray = (ArrayList<String>) documentSnapshot.get("userArray");
+
+
+//                            if (splitAmount == null) {
+//                                for (int i = 0; i < user.size(); i++) {
+//                                    splitAmount.put(userArray.get(i), total / userArray.size());
+//                                    splitUser.add(userArray.get(i));
+//                                }
+//                            }
+
+                            data.put("billId", doc.getId());
+                            data.put("billName", tvBillName.getText().toString());
+                            data.put("payer", auth.getCurrentUser().getUid());
+                            data.put("price", total);
+                            data.put("createTime", Timestamp.now());
+//                            data.put("splitAmount", splitAmount);
+                            data.put("splitUser", splitUser);
+
+                            db.collection("Groups").document(groupId).collection("Payment").document(doc.getId()).set(data);
+
+                            //Map 1 = userArray | Map 2 = splitAmount (Added to user)
+//                                for (String key : splitAmount.keySet()) {
+//                                    Double oldValue = user.get(key);
+//                                    Double valueToAdd = splitAmount.get(key);
+//                                    user.put(key, oldValue + valueToAdd);
+//                                }
+
+                            db.collection("Groups").document(groupId).update("user", user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+//                                    Toast.makeText(getApplicationContext(), "Added Expense", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+
+
                         }
                     }
                 });
+            }
 
-                //userArray.get();
+        });
 
-                double temp = Double.parseDouble(tvPrice.getText().toString());
-
-
-                data.put("billId", doc.getId());
-                data.put("billName", tvBillName.getText().toString());
-                data.put("payer", auth.getCurrentUser().getUid());
-                data.put("price", temp);
-                data.put("createTime", Timestamp.now());
-                data.put("splitAmount", userArray);
-
-                db.collection("Groups").document(groupId).collection("Payment").document(doc.getId()).set(data);
-
+        btnSplit = findViewById(R.id.btnSplit);
+        btnSplit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(ExpenseAddNew.this, );
 
             }
         });
