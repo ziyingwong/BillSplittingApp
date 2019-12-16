35c23352f75345cbe82b67c7095c2187b0775583
AMIRUL <muhdamirulsalim@gmail.com>
2019-12-16 09:52:53 +0800

    method of split update

--- app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
+++ app/src/main/java/com/example/billsplittingapp/ExpenseAddNew.java
@@ -13,23 +13,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.google.firebase.firestore.remote.WatchChange.WatchTargetChangeType.Added;
@@ -122,7 +115,8 @@ public class ExpenseAddNew extends AppCompatActivity {
 
                            // Map 1 = userArray | Map 2 = splitAmount (Added to user)
                                 for (String key : splitAmount.keySet()) {
-                                    Double oldValue = user.get(key);
+                                    Object objectamount = user.get(key);
+                                    Double oldValue = Double.parseDouble(objectamount.toString());
                                     Double valueToAdd = splitAmount.get(key);
                                     user.put(key, oldValue + valueToAdd);
                                 }
